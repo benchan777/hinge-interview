c442b4a6baf989e89710f2c43b335b8122bf497d
benchan777 <benchan777@gmail.com>
2025-05-31 00:23:14 -0700

    part 2 - data modeling

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import './index.css'
+import data from './data.json'
 
 export default function Tree() {
-    return (
-      <div className="tree">
-        mammals<br/>
-        <div className="one-indent">
-          cheetah
-        </div>
-        <div className="one-indent">
-          bear
-        </div>
-        <div className="two-indent">
-          lion
-        </div>
-        <div className="two-indent">
-          dog
-        </div>
-        <div className="three-indent">
-          elephant
-        </div>
-        <div className="one-indent">
-          ape
-        </div>
-      </div>
-    )
+
+  /**
+   * Parse animal data from data.json and generate HTML elements with the
+   * appropriate indentation for each animal. The data in the json file is formatted
+   * where the key is the name of the animal and the value is its indentation level.
+   * @returns HTML including all animals with the proper styling for each animal
+   */
+  const buildTree = () => {
+    const elements = [];
+
+    for (const animalType in data) {
+      const animals = data[animalType]
+
+      for (const [animal, column] of Object.entries(animals)) {
+        elements.push(
+        <div key={animal} style={{textIndent: `${column}em`}}>
+          {animal}
+        </div>)
+      }
+    }
+
+    return elements;
+  };
+
+  return (
+    <div className="tree">
+      mammals<br/>
+      {buildTree()}
+    </div>
+  )
 }
